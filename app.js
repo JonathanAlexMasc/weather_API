0968f3c477ebab02c33e95174acea89f87b369d3
Jonathan Mascarenhas <Jonathan.Mascarenhas@mines.sdsmt.edu>
2022-12-24 16:18:30 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Tutorial by http://youtube.com/CodeExplained
+// api key : 82005d27a116c2880c8f0fcb866998a0
+
+const iconElement = document.querySelector(".weather-icon");
+
+const weather = {
+
+    temperature : {
+        value : 18,
+        unit : "celsius"
+    },
+
+    description : "few clouds", 
+    iconId : "O1d", 
+    city : "London",
+    country : "GB"
+};
