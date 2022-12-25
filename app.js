df6f39a056bf1f6762f488b36588fa33f9836d1f
Jonathan Mascarenhas <Jonathan.Mascarenhas@mines.sdsmt.edu>
2022-12-25 14:48:15 -0700

    input functionality availible

--- app.js
+++ app.js
@@ -7,97 +7,51 @@ const tempElement = document.querySelector(".temperature-value");
 const descElement = document.querySelector(".temperature-description");
 const locationElement = document.querySelector(".location");
 const notificationElement = document.querySelector(".notification");
+const userInput = document.querySelector(".userInput");
 
-// App data
-const profile = {};
+let username;
 
-// weather.temperature = {
-//     unit : "celsius"
-// }
-
-// APP CONSTS AND VARS
-const KELVIN = 273;
-// API KEY
-const key = "82005d27a116c2880c8f0fcb866998a0";
-
-
-// // CHECK IF BROWSER SUPPORTS GEOLOCATION
-// if('geolocation' in navigator){
-//     navigator.geolocation.getCurrentPosition(setPosition, showError);
-// }else{
-//     notificationElement.style.display = "block";
-//     notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
-// }
-
-// // SET USER'S POSITION
-// function setPosition(position){
-//     let latitude = position.coords.latitude;
-//     let longitude = position.coords.longitude;
-    
-//     getWeather(latitude, longitude);
-// }
-
-// // SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
-// function showError(error){
-//     notificationElement.style.display = "block";
-//     notificationElement.innerHTML = `<p> ${error.message} </p>`;
-// }
-
-getWeather();
-
-function strip(number)
+document.getElementById("myButton").onclick = function getName()
 {
-    return (parseFloat(number).toPrecision(3));
-}
-
-// GET WEATHER FROM API PROVIDER
-function getWeather(){
-    let api = `https://api.genderize.io?name=alex&country_id=US`;
+    username = document.getElementById("myText").value;
+    console.log(username);
     
-    fetch(api)
-        .then(function(response){
-            let data = response.json();
-            return data;
-        })
-        .then(function(data){
-            
-            profile.name = data.name;
-            profile.gender = data.gender;
-            profile.probability = data.probability * 100;
-            profile.count = data.count;
+    // App data
+    const profile = {};
+    getWeather();
 
-        })
-        .then(function(){
-            displayWeather();
-        });
-}
-
-// DISPLAY WEATHER TO UI
-function displayWeather(){
-    iconElement.innerHTML = `Name : ${profile.name}`;
-    tempElement.innerHTML = `Gender : ${profile.gender}`;
-    descElement.innerHTML = `Odds : ${profile.probability}%`;
-    locationElement.innerHTML = `Number of Occurences : ${profile.count}`;
-}
-
-// // C to F conversion
-// function celsiusToFahrenheit(temperature){
-//     return (temperature * 9/5) + 32;
-// }
-
-// // WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
-// tempElement.addEventListener("click", function(){
-//     if(weather.temperature.value === undefined) return;
-    
-//     if(weather.temperature.unit == "celsius"){
-//         let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
-//         fahrenheit = Math.floor(fahrenheit);
+    // GET WEATHER FROM API PROVIDER
+    function getWeather(){
         
-//         tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
-//         weather.temperature.unit = "fahrenheit";
-//     }else{
-//         tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-//         weather.temperature.unit = "celsius"
-//     }
-// });
+        let api = `https://api.genderize.io?name=${username}&country_id=US`;
+        
+        fetch(api)
+            .then(function(response){
+                let data = response.json();
+                return data;
+            })
+            .then(function(data){
+                profile.name = data.name;
+                profile.gender = data.gender;
+                profile.probability = data.probability * 100;
+                profile.count = data.count;
+
+            })
+            .then(function(){
+                displayWeather();
+            });
+    }
+
+    // DISPLAY WEATHER TO UI
+    function displayWeather(){
+        iconElement.innerHTML = `Name : ${profile.name}`;
+        tempElement.innerHTML = `Gender : ${profile.gender}`;
+        descElement.innerHTML = `Odds : ${profile.probability}%`;
+        locationElement.innerHTML = `Number of Occurences : ${profile.count}`;
+    }
+
+}
+
+
+
 
